880738cad9c0b3b180b2a8113dedd7fec3c1adcc
Gilang Adi S <gilangas@outlook.com>
2021-11-14 16:17:45 +0700

    add: Example Code

---
+++ example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	gtranslate "github.com/gilang-as/google-translate"
+	"log"
+)
+
+func main()  {
+	value := gtranslate.Translate{
+		Text: "Halo Dunia",
+		//From: "id",
+		To: "en",
+	}
+	translated, err := gtranslate.Translator(value)
+	if err != nil {
+		panic(err)
+	}else{
+		prettyJSON, err := json.MarshalIndent(translated, "", "\t")
+		if err != nil {
+			log.Fatal("Failed to generate json", err)
+		}
+		fmt.Println(string(prettyJSON))
+	}
+}
